d8f8567208b08d006fdf9afdb5da236f8b859002
blacksonic <soos.gabor86@gmail.com>
2019-09-13 19:55:09 +0200

    feat(header): implement event handling

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,14 @@ import { List } from './List';
 import { Footer } from './Footer';
 
 export class App extends Component {
+  onCreate(name) {
+    console.log('on create', name);
+  }
+
   render() {
     return (
       <section className="todoapp">
-        <Header />
+        <Header onSave={(name) => this.onCreate(name)} />
         <List />
         <Footer />
       </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const ENTER_KEY = 13;
 
 export class Header extends Component {
+  static propTypes = {
+    onSave: PropTypes.func.isRequired
+  };
+
+  state = {
+    name: ''
+  };
+
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit(event) {
+    if (event.which === ENTER_KEY) {
+      this.props.onSave(this.state.name);
+      this.setState({ name: '' });
+    }
+  }
+
   render() {
     return (
       <header className="header">
         <h1>todos</h1>
-        <input className="new-todo" placeholder="What needs to be done?" value="" />
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus={true}
+          value={this.state.name}
+          onChange={(e) => this.handleChange(e)}
+          onKeyDown={(e) => this.handleSubmit(e)}
+        />
       </header>
     );
   }
