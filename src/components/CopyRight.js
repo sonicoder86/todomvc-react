b9483f37c9cf8038e648db977867d367eaa878fe
blacksonic <soos.gabor86@gmail.com>
2020-02-02 16:04:47 +0100

    feat(test): setup testing

--- src/components/CopyRight.js
+++ src/components/CopyRight.js
@@ -4,7 +4,7 @@ export class CopyRight extends Component {
   render() {
     return (
       <footer className="info">
-        <p>Double-click to edit a todo</p>
+        <p data-testid="instruction">Double-click to edit a todo</p>
         <p>Created by <a href="http://github.com/blacksonic/">blacksonic</a></p>
         <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
       </footer>
