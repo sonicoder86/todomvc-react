8bcda8329ebda9985ca78986c32a039ad1c0eb11
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-04 09:53:59 +0000

    Deploy blacksonic/todomvc-react to github.com/blacksonic/todomvc-react.git:gh-pages

---
+++ precache-manifest.2019dd74c90ea75dc6d615fd58d51118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983c7aab9e7fdf179a18cf87580132c4",
+    "url": "https://blacksonic.github.io/todomvc-react/index.html"
+  },
+  {
+    "revision": "23344fa5c1aa3c196deb",
+    "url": "https://blacksonic.github.io/todomvc-react/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "23344fa5c1aa3c196deb",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/2.ddb5cbb8.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/2.ddb5cbb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e682a7f7c4e1e42bfb99",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/main.7b6eb684.chunk.js"
+  },
+  {
+    "revision": "e4f1349811895a07a49a",
+    "url": "https://blacksonic.github.io/todomvc-react/static/js/runtime-main.90a63d6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3abc3b064b264aeecb5402c2e6dcb4c3.js"
+  "https://blacksonic.github.io/todomvc-react/precache-manifest.2019dd74c90ea75dc6d615fd58d51118.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://blacksonic.github.io/todomvc-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
