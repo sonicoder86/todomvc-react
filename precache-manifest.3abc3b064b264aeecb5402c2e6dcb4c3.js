10cb9dd27e7463fb3154c3f886bbc3570f9eb808
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-04 09:46:59 +0000

    Deploy blacksonic/todomvc-react to github.com/blacksonic/todomvc-react.git:gh-pages

---
+++ precache-manifest.3abc3b064b264aeecb5402c2e6dcb4c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a5b001db0da92ac014e9791b7239e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23344fa5c1aa3c196deb",
+    "url": "/static/css/2.f124e354.chunk.css"
+  },
+  {
+    "revision": "23344fa5c1aa3c196deb",
+    "url": "/static/js/2.ddb5cbb8.chunk.js"
+  },
+  {
+    "revision": "a5da5d53dfba093fa63375799ffeddd6",
+    "url": "/static/js/2.ddb5cbb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e682a7f7c4e1e42bfb99",
+    "url": "/static/js/main.7b6eb684.chunk.js"
+  },
+  {
+    "revision": "074d52f9e734e37161d9",
+    "url": "/static/js/runtime-main.e6b8c594.js"
+  }
+]);
