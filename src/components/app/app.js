fd81f04b2f09760e45e4ce06965952370f52ed3e
blacksonic <soos.gabor86@gmail.com>
2020-12-27 13:49:35 +0100

    remove react imports

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { TodoLocal } from '../../services/todo-local';
 import { withStateAndDispatch } from '../../store';
